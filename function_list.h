c2e7c05659c7c84898b76a52b089548fec81919f
ChaseR34 <33583805+ChaseR34@users.noreply.github.com>
2017-11-26 13:35:36 -0700

    Add files via upload

---
+++ function_list.h
@@ -0,0 +1,47 @@
+#ifndef suffixTree.h
+#define suffixTree_h
+
+struct suffixNode{
+	suffixNode * A;
+	suffixNode * C;
+	suffixNode * G;
+	suffixNode * T;
+	suffixNode * N;
+	
+	int start;
+	int end;
+
+};
+
+class Suffix Tree{
+	
+		private char * subject;
+			int G;
+		
+	Node * root;
+	void set_subject_into_tree(char *, int size);
+			
+		public:
+				
+			void suffix_tree();
+			void suffix_tree(char *, int size);
+			void ~suffix_tree();
+
+
+
+			void insert();
+			void serarch();
+			struct *Node getNode();
+
+	bool querry_trace(char *, int size);
+
+
+
+}
+
+
+int chartoASCII(char c);
+
+
+#endif
+

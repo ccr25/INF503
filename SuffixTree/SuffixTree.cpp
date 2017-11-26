497da92d37d6b33b2593f560c0619d9985ee35c7
ccr25 <bingler25@gmail.com>
2017-11-26 15:03:42 -0700

    not functional, but testing it

--- SuffixTree/SuffixTree.cpp
+++ SuffixTree/SuffixTree.cpp
@@ -7,6 +7,19 @@
 #define num 50000 // number of 36-mers
 #define arraysize(a) sizeof(a)/sizeof(a[0])
 #define alphabeta (5) //possible node for children.
+
+//constructor
+void suffix_Tree(const char *contig, int contigSize) {
+    
+    struct suffixNode *root = createNode();
+    
+    // store them in a trie
+    for (i = 0; i < arraysize(contigSize); i++)
+       {
+	   	count=insert(root, contigSize[i],count);  
+		} 
+}
+
 int chartoASCII(c){
 	if ((int)c==65){
 		return 0;
@@ -18,10 +31,10 @@ int chartoASCII(c){
 		return 3;
 	}
 }
-struct Node *createNode(void)
+struct suffixNode *createNode(void)
 {
-    struct Node *pN = NULL;
-    pN = (struct Node *)malloc(sizeof(struct Node));
+    struct suffixNode *pN = NULL;
+    pN = (struct suffixNode *)malloc(sizeof(struct suffixNode));
     if (pN)
     {
         int i;
@@ -34,12 +47,12 @@ struct Node *createNode(void)
 
 // insert a new sequence to the tree
 // We don't need int count in our project, it counts number of nodes in the tree.
-void insert(struct Node *root, const char *contig)
+void insert(struct suffixNode *root, const char *contig)
 {
     int i;
     int length = strlen(contig);
     int index;
-    struct Node *p = root;
+    struct suffixNode *p = root;
     for (i = 0; i < length; i++)
     {
         index = chartoASCII(contig[i]);
@@ -50,3 +63,30 @@ void insert(struct Node *root, const char *contig)
     }
     p->isend = true;
 } 
+
+// search a sequence if it is presneted in the tree or not. return 0 for not presented, 1 for presented;
+bool search(struct suffixNode *root, const char *contig, int mismatchValue)
+{
+    int i;
+    int count = 0;
+    int length = strlen(contig);
+    int index;
+    struct suffixNode *p = root;
+    for (i = 0; i < length; i++)
+    {
+        index = chartoASCII(contig[i]);
+ 
+        if (!p->children[index])
+	    count++;
+	    if (count > mismatchValue)
+                 return false;
+ 
+        p = p->children[index];
+    }
+    return (p!= NULL && p->isend);
+}
+
+//destructor
+suffix_tree::~suffix_tree() {
+	delete []root;
+}

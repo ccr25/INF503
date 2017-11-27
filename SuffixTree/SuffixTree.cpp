54b6ef746237225a03b0c1783e94b8c4909c87f0
ccr25 <bingler25@gmail.com>
2017-11-26 17:46:03 -0700

    Works for perfect matching

--- SuffixTree/SuffixTree.cpp
+++ SuffixTree/SuffixTree.cpp
@@ -1,45 +1,87 @@
+//
+// Created by Chandler Roe on 11/26/17.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <fstream>
+#include "suffixTree.h"
+#include <iostream>
 //#define FILENAME "DENV2.txt"
 #define N 100
 #define num 50000 // number of 36-mers
 #define arraysize(a) sizeof(a)/sizeof(a[0])
-#define alphabeta (5) //possible node for children.
+//#define alphabet (5) //possible node for children.
+
+
+using namespace std;
 
 //constructor
-void suffix_Tree(const char *contig, int contigSize) {
-    
-    struct suffixNode *root = createNode();
-    
-    // store them in a trie
-    for (i = 0; i < arraysize(contigSize); i++)
-       {
-	   	count=insert(root, contigSize[i],count);  
-		} 
+void Suffix_Tree::suffix_tree(char * contig, int contigSize, suffixNode *root) {
+    cout<<"contig = "<<contig<<"\n";
+    //suffixNode *root = Suffix_Tree::createNode();
+    // store them in a tree
+        for (int i = 0; i < contigSize; i++) {
+            char * splitContig = new char [i+1];
+
+
+            strncpy(splitContig, contig + contigSize -1 -i, i+1);
+            printf("%s\n", splitContig);
+            Suffix_Tree::insert(root, splitContig, i + 1);
+        }
 }
 
-int chartoASCII(c){
-	if ((int)c==65){
-		return 0;
-	} else if ((int)c==67){
-		return 1;
-	} else if ((int)c==71){
-		return 2;
-	} else if ((int)c==84){
-		return 3;
-	}
+char * Suffix_Tree::readFileFunction() {
+
+    string line;
+
+    //string subLineToAddToNode;
+    ifstream inReadsFile("/Users/croe/Desktop/viralDatabase.fasta");
+    if (inReadsFile.is_open()) {
+        while (!inReadsFile.eof()) {
+            while (getline(inReadsFile, line)) {
+                if (line[0] != '>') {
+                    char * subLineToReturn = new char[13];
+                    strcat(subLineToReturn, line.c_str());
+                    std::cout<<"sub line: "<<subLineToReturn<<"\n";
+                    return subLineToReturn;
+                }
+            }
+        }
+    }
 }
-struct suffixNode *createNode(void)
+
+
+
+int chartoASCII(char c){
+    if ((int)c==65){
+        return 0;
+    }
+    else if ((int)c==67){
+        return 1;
+    }
+    else if ((int)c==71){
+        return 2;
+    }
+    else if ((int)c==84){
+        return 3;
+    }
+    else if ((int)c==78){
+        return 4;
+    }
+
+}
+suffixNode * Suffix_Tree::createNode()
 {
     struct suffixNode *pN = NULL;
     pN = (struct suffixNode *)malloc(sizeof(struct suffixNode));
     if (pN)
     {
         int i;
-        pN->isend = false;	
-        for (i = 0; i < alphabeta; i++)
+        pN->isend = false;
+        for (i = 0; i < alphabetSize; i++)
             pN->children[i] = NULL;
     }
     return pN;
@@ -47,10 +89,10 @@ struct suffixNode *createNode(void)
 
 // insert a new sequence to the tree
 // We don't need int count in our project, it counts number of nodes in the tree.
-void insert(struct suffixNode *root, const char *contig)
-{
+void Suffix_Tree::insert(struct suffixNode *root, const char *contig, int length) {
+
     int i;
-    int length = strlen(contig);
+    //int length = strlen(contig);
     int index;
     struct suffixNode *p = root;
     for (i = 0; i < length; i++)
@@ -58,35 +100,36 @@ void insert(struct suffixNode *root, const char *contig)
         index = chartoASCII(contig[i]);
         if (!p->children[index]){
             p->children[index] = createNode();
-		}
+        }
         p = p->children[index];
     }
     p->isend = true;
-} 
+}
 
 // search a sequence if it is presneted in the tree or not. return 0 for not presented, 1 for presented;
-bool search(struct suffixNode *root, const char *contig, int mismatchValue)
+bool Suffix_Tree::search(struct suffixNode *root, const char *contig, int mismatchValue, int length)
 {
     int i;
     int count = 0;
-    int length = strlen(contig);
     int index;
     struct suffixNode *p = root;
     for (i = 0; i < length; i++)
     {
         index = chartoASCII(contig[i]);
- 
-        if (!p->children[index])
-	    count++;
-	    if (count > mismatchValue)
-                 return false;
- 
+
+        if (!p->children[index]) {
+            count++;
+            cout<<"count equals: "<<count<<"\n";
+            if (count > mismatchValue) {
+                return false;
+            }
+            }
         p = p->children[index];
     }
     return (p!= NULL && p->isend);
 }
 
 //destructor
-suffix_tree::~suffix_tree() {
-	delete []root;
-}
+/*~Suffix_Tree::suffix_tree() {
+    delete []root;
+}*/

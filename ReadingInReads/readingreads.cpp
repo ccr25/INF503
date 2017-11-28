1f957787d80bfb9ccfb3d84ab7b03693ad12b6e0
ccr25 <bingler25@gmail.com>
2017-11-27 20:05:04 -0700

    broke up into main, h, and cpp files

--- ReadingInReads/readingreads.cpp
+++ ReadingInReads/readingreads.cpp
@@ -1,7 +1,9 @@
+//
+// Created by Chandler Roe on 11/27/17.
+//
+
 #include <iostream>
 #include <fstream>
-#include <math.h>
-
 
 using namespace std;
 
@@ -10,7 +12,6 @@ struct readNode {
     int readNumber;
     readNode * next;
     string readSeedSequence;
-    //long double radix_value;
 };
 
 readNode * CreateReadNode() {
@@ -38,44 +39,12 @@ int retrieveNumberOfLines () {
     return number_of_lines;
 }
 
-/*long double radix_converstion(string instring, int kmerSize) {
-    long double str_value, base_value;
-    //cout<<"instring: "<<"\n";
-    //cout <<"radix_conversion_string: "<< instring << "\n";
-    //cout << instring.length() << "\n";
-    str_value = 0;
-    for (int i=0; i < instring.length(); i++)
-        //cout << i << '\n';
-        if (instring[i] == 'A') {
-            base_value = (0 * pow(4, (kmerSize-i)));
-            str_value += base_value;
-        }
-        else if (instring[i] == 'C') {
-            base_value = (1 * pow(4, (kmerSize-i)));
-            str_value += base_value;
-        }
-        else if (instring[i] == 'G') {
-            base_value = (2 * pow(4, (kmerSize-i)) );
-            str_value += base_value;
-        }
-        else if (instring[i] == 'T') {
-            base_value = (3 * pow(4, (kmerSize-i)));
-            str_value += base_value;
-        }
-        else {
-            cout<<"instring i: "<<instring[i]<<"\n";
-            cout<<"i is: "<<i<<"\n";
-            cout << "test_poop\n";//' << '\n';
-        }
-    return str_value;
-}*/
-
 readNode ** populateReadsFileArray( readNode ** readsFile, int kmerSize, int readLength) {
     string line;
     long double radix_value_to_add;
     int readNumber = 1;
-    int countfirstNode = 0;
-    int countNextNode = 0;
+    //int countfirstNode = 0;
+    //int countNextNode = 0;
     int steppingSize = (readLength - kmerSize) + 1;
     string subLineToAddToNode;
     ifstream inReadsFile("/Users/croe/Desktop/hw_dataset.fa");
@@ -84,18 +53,15 @@ readNode ** populateReadsFileArray( readNode ** readsFile, int kmerSize, int rea
             while (getline(inReadsFile, line)) {
                 if (line[0] != '>') {
                     int counter = 0;
-                        while (counter < steppingSize) {
-                            subLineToAddToNode = line.substr(counter, kmerSize);
-                            //radix_value_to_add = radix_converstion(subLineToAddToNode, kmerSize);
-                            readNode * newReadNode = CreateReadNode();
-                            //newReadNode->radix_value = radix_value_to_add;
-                            //cout<<"radix value to add: "<<radix_value_to_add<<"\n";
-                            newReadNode->readSeedSequence = subLineToAddToNode;
-                            cout<<"read kmer sequence: "<<subLineToAddToNode<<"\n";
-                            newReadNode->readNumber= readNumber;
-                            cout<<"read number: "<<readNumber<<"\n";
-                            counter++;
-                        }
+                    while (counter < steppingSize) {
+                        subLineToAddToNode = line.substr(counter, kmerSize);
+                        readNode * newReadNode = CreateReadNode();
+                        newReadNode->readSeedSequence = subLineToAddToNode;
+                        cout<<"read kmer sequence: "<<subLineToAddToNode<<"\n";
+                        newReadNode->readNumber= readNumber;
+                        cout<<"read number: "<<readNumber<<"\n";
+                        counter++;
+                    }
                     readNumber++;
                 }
             }
@@ -103,19 +69,4 @@ readNode ** populateReadsFileArray( readNode ** readsFile, int kmerSize, int rea
     }
     inReadsFile.close();
     return readsFile;
-}
-
-int main() {
-
-    //Figure out the number of reads in a file
-    int numberOfLines = retrieveNumberOfLines();
-    int readLength = 50;
-    //cout<<"Number of lines is: "<<numberOfLines<<"\n";
-    int kmerSize = 30;
-    int numberOfReadSeeds = (numberOfLines * kmerSize );
-
-    readNode ** slavasReadsNodeArray = new readNode * [numberOfReadSeeds];
-    populateReadsFileArray(slavasReadsNodeArray, kmerSize, readLength);
-
-    return 0;
 }

30657772f228f2682edccbfccea0396db8c6b61e
hp263 <32024225+hp263@users.noreply.github.com>
2017-11-29 11:16:44 +0800

    Update read_store.cpp

--- read_store.cpp
+++ read_store.cpp
@@ -13,34 +13,34 @@ int count=0;
 int size;
 char head[40][60];
 char geno[40][40];
-char buffer[40];
+string buffer2 ="";
+char buffer[60] ="";
+char buffer1[60] ="";
 while(file >> content) {
 	x=content.find(">");
 	//printf("%d\n",x);
 
 if (x==0){
-	
 	strcpy(geno[count],content.c_str());
 	//cout << geno[count] <<"\n";
-	//buffer[60]=NULL;
+	memset(buffer1, 0, 40);
 	count++;
 	
 } else if (x!=0){
 	//x=content.find(">");
-
+	memset(buffer1, 0, 40);
 	//strcpy(head[count],content.c_str());
 	strcpy(buffer,content.c_str());
-
+	strcpy(buffer1,buffer);
+	//strtok(buffer1,"T");
 	//size= strlen(head[count]);
 	//cout << buffer <<"\n";
-	//cout << "size of this sting is "<<size <<"\n";
-		
+	//cout << "size of this sting is "<<size <<"\n";		
 }
-	strcat(head[count],buffer);
-	
+	strcat(head[count],buffer1);
 }
-strcat(head[count],buffer);
-cout << head[2] <<"\n";
+//strcat(head[count],buffer);
+cout << head[13] <<"\n";
 //printf("number of genos is %d",count);
 file.close(); 
 return 0;

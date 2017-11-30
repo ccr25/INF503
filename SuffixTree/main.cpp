67477a707d5ff438944ac454485ef8fc7a16806a
hp263 <32024225+hp263@users.noreply.github.com>
2017-11-30 13:06:00 +0800

    takes multiple genes into tree.

--- SuffixTree/main.cpp
+++ SuffixTree/main.cpp
@@ -12,18 +12,29 @@ using namespace std;
 
 int main() {
 
+    int anotherCool;
     int cool;
     suffixNode * root= Suffix_Tree::createNode();
-    const char * subStr = "TGC";
-    char * tester = new char[13];
+    const char * subStr = "TATACTCTCTATATAACGTGCATGCATCGA";
+    const char * subStrTst2 = "ATATTCCCTCTCTCCCCCTTTATATATATA";
+    char * tester2 = "CTATCTCTAATTATATTCCCTCTCTCCCCCTTTATATATATA";
+    char * tester = new char[36];
+    tester = "TTTCTATATACTCTCTATATAACGTGCATGCATCGA";
+    Suffix_Tree::suffix_tree(tester, 36, root);
+    Suffix_Tree::suffix_tree(tester2, 42, root);
+    cool = Suffix_Tree::search(root, subStr, 0, 30);
+    anotherCool = Suffix_Tree::search(root, subStrTst2, 0, 30);
+    cout<<"search number: "<<cool<<"\n";
+    cout<<"search anotherCool is: "<<anotherCool<<"\n";
+
     int numbRGenomes;
     int * intArray;
     //std::ifstream inReadFile("/Users/croe/Desktop/viralDatabase.fasta");
     //open the multifasta database and count how many genomes are going to be loaded into the tree, return that number
     numbRGenomes = readNumberOfGenomesInDatabase();
-    cout<<"Number of Genomes in Database: "<<numbRGenomes<<"\n";
+    //cout<<"Number of Genomes in Database: "<<numbRGenomes<<"\n";
 
-    intArray = fasta_charcount(numbRGenomes);
+   /* intArray = fasta_charcount(numbRGenomes);
     char ** header_array = new char *[numbRGenomes];
     char ** count_array = new char * [numbRGenomes];
     for (int i= 0; i < numbRGenomes; i++) {
@@ -44,7 +55,7 @@ int main() {
     tester = Suffix_Tree::readFileFunction();
     Suffix_Tree::suffix_tree(tester, 13, root);
     cool = Suffix_Tree::search(root, subStr, 1, 3);
-    //cout<<"search = "<<cool<<"\n";
+    //cout<<"search = "<<cool<<"\n";*/
 
 
 }

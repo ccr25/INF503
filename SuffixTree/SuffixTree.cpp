c7a7c8afdb696170a9392228d6fcad9d04694d86
ccr25 <bingler25@gmail.com>
2017-12-03 20:13:00 -0700

    working on turning trie to tree

--- SuffixTree/SuffixTree.cpp
+++ SuffixTree/SuffixTree.cpp
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <stdbool.h>
 #include <fstream>
-#include "suffixTree.h"
+#include "SuffixTree.h"
 #include <iostream>
 #include <vector>
 
@@ -28,15 +28,15 @@ void concatGenomes(int grt_num, char ** header_array, char ** count_array, int *
         while (std::getline(fin, line)) {
             //printf("line = %s\n, length = %lu\n",line, strlen(line));
             if (line[0] == '>') {
-                cout<<"line: "<<line<<"\n";
+                //cout<<"line: "<<line<<"\n";
                 string line2 = line;
                 strcpy(header_array[lin_count], line2.c_str());
-                found = strchr(header_array[lin_count], '_');
+                found = strchr(header_array[lin_count], '-');
                 iD = found - header_array[lin_count] + 1;
                 size = std::strlen(found);
                 strncpy(IDBuffer, header_array[lin_count] + iD, size);
                 ID_array[lin_count] = atoi(IDBuffer);
-                cout<<"ID is "<<ID_array[lin_count]<<"\n";
+                //cout<<"ID is "<<ID_array[lin_count]<<"\n";
 
                 //cout<<"header array is: "<<header_array[lin_count]<<"\n";
                 lin_count++;
@@ -72,32 +72,32 @@ int * fasta_charcount(int grt_num) {
 
         while (getline(inDatabase, line)) {
 
-            printf("line = %s\n, length = %lu\n", line.c_str(), std::strlen(line.c_str()));
+            //printf("line = %s\n, length = %lu\n", line.c_str(), std::strlen(line.c_str()));
 
             if (line[0] == '>') {
                 lin_count++;
             } else {
-                printf("line_count = %d\n", lin_count);
+                //printf("line_count = %d\n", lin_count);
                 count_array[lin_count - 1] = count_array[lin_count - 1] + std::strlen(line.c_str());
             }
         }
     }
-        inDatabase.seekg(0, ios::beg);
+    inDatabase.seekg(0, ios::beg);
 
-        return count_array;
-    }
-
+    return count_array;
+}
+//TODO::make sure to change the value back to 29!!
 //constructor creates an array containing all possible kmers for the genome and populates the array.
-void Suffix_Tree::suffix_tree(char * contig, int contigSize, suffixNode *root) {
+void Suffix_Tree::suffix_tree(char * contig, int contigSize, suffixNode *root, int ID, char ** genome_array ) {
     //cout<<"contig = "<<contig<<"\n";
     //suffixNode *root = Suffix_Tree::createNode();
     // store them in a tree
-        for (int i = 29; i < contigSize; i++) {
-            char * splitContig = new char [i+1];
-            strncpy(splitContig, contig + contigSize -1 -i, i+1);
-            printf("%s\n", splitContig);
-            Suffix_Tree::insert(root, splitContig, i + 1);
-        }
+    for (int i = 0; i < contigSize; i++) {
+        char * splitContig = new char [i+1];
+        strncpy(splitContig, contig + contigSize -1 -i, i+1);
+        //printf("%s\n", splitContig);
+        Suffix_Tree::insert(root, splitContig, i + 1, ID, i, genome_array);
+    }
 }
 
 //function to read in the database multifasta file and return the number of genomes in the database based on '>'.
@@ -119,22 +119,24 @@ int readNumberOfGenomesInDatabase() {
 }
 
 //function to read in the database multifasta file and populate the 2D array with each genome.
-char * Suffix_Tree::readFileFunction() {
+/*char * Suffix_Tree::readFileFunction() {
     string line;
     ifstream inReadsFile("/Users/croe/Desktop/viralDatabase.fasta");
     if (inReadsFile.is_open()) {
         while (!inReadsFile.eof()) {
             while (getline(inReadsFile, line)) {
                 if (line[0] != '>') {
-                    char * subLineToReturn = new char [13];
+                    char * subLineToReturn = new char [20000];
                     strcat(subLineToReturn, line.c_str());
                     //std::cout<<"sub line: "<<subLineToReturn<<"\n";
+                    cout<<"Sub line to return: "<<subLineToReturn<<"\n";
                     return subLineToReturn;
                 }
             }
         }
     }
-}
+
+}*/
 
 //function converts nucleotide bases 'A', 'C', 'G', 'T' into ascii values for easier comparison. Returns ascii value.
 int charToASCII(char c){
@@ -172,29 +174,54 @@ suffixNode * Suffix_Tree::createNode()
 
 // insert a new sequence to the tree
 // We don't need int count in our project, it counts number of nodes in the tree.
-void Suffix_Tree::insert(struct suffixNode *root, const char *contig, int length) {
+void Suffix_Tree::insert(struct suffixNode *root, const char *contig, int length, int ID, int startValue, char ** genome_array) {
 
     int i;
     //int length = strlen(contig);
     int index;
     struct suffixNode *p = root;
-    for (i = 0; i < length; i++)
-    {
+    for (i = 0; i < length; i++) {
         index = charToASCII(contig[i]);
-        if (!p->children[index]){
+        int keepTrackOfNode = charToASCII(genome_array[p->ID][p->start + i]);
+        if (p->isend == false) {
+            for (int j = p->start; j < p->end; j++) {
+                if (genome_array[p->ID][j] == contig[i]) {
+                    i++;
+                }
+                else {
+                    p->children[keepTrackOfNode] = createNode();
+                    p->children[keepTrackOfNode]->ID = p->ID;
+                    p->children[keepTrackOfNode]->start = p->start + j;
+                    p->children[keepTrackOfNode]->isend = true;
+                    p->isend = false;
+                    p->end = j;
+                    break;
+                }
+            }
+        }
+        if (!p->children[index]) {
             p->children[index] = createNode();
+            p->children[index]->start = startValue;
+            if (p->isend == true) {
+                int keepTrackOfNode = charToASCII(genome_array[p->ID][p->start + i]);
+                for (int j = p->start; j < p->end; j++) {
+                    if (genome_array[p->ID][j] == contig[i]) {
+                        i++;
+                    } else {
+                        p->children[keepTrackOfNode] = createNode();
+                        p->children[keepTrackOfNode]->ID = p->ID;
+                        p->children[keepTrackOfNode]->start = p->start + j;
+                        p->children[keepTrackOfNode]->isend = true;
+                        p->isend = false;
+                        p->end = j;
+                        break;
+                    }
+                }
+            }
         }
         p = p->children[index];
     }
-    p->isend = true;
 }
-
-/*void storeTaxaIDonTerminalNode(struct suffixNode *p, ) {
-
-    if (p->isend == true) {
-    }
-}*/
-
 // search a sequence if it is presneted in the tree or not. return 0 for not presented, 1 for presented;
 bool Suffix_Tree::search(struct suffixNode *root, const char *contig, int mismatchValue, int length)
 {
@@ -212,10 +239,74 @@ bool Suffix_Tree::search(struct suffixNode *root, const char *contig, int mismat
             if (count > mismatchValue) {
                 return false;
             }
-            }
+        }
         p = p->children[index];
     }
-    return (p!= NULL && p->isend);
+    //return (p!= NULL && p->isend);
+    return true;
+}
+
+struct readNode {
+    int locationInRead;
+    int readNumber;
+    readNode * next;
+    string readSeedSequence;
+};
+
+readNode * CreateReadNode() {
+    readNode *newNode = new readNode;
+    newNode->next = NULL;
+    return newNode;
+}
+
+//function will retrieve the number of lines in the multifasta file
+int retrieveNumberOfLines () {
+    string line;
+    int number_of_lines = 0;
+    //ifstream inReadsFile("/Users/croe/Desktop/test_reads.fasta");
+   /* if (inReadsFile.is_open()) {
+        while (!inReadsFile.eof()) {
+            while (getline(inReadsFile, line)) {
+                if (line[0] != '>') {
+                    number_of_lines++;
+                }
+            }
+        }
+    }
+    inReadsFile.close();
+    return number_of_lines;*/
+}
+
+readNode ** populateReadsFileArray( readNode ** readsFile, int kmerSize, int readLength) {
+    string line;
+    long double radix_value_to_add;
+    int readNumber = 1;
+    //int countfirstNode = 0;
+    //int countNextNode = 0;
+    int steppingSize = (readLength - kmerSize) + 1;
+    string subLineToAddToNode;
+    ifstream inReadsFile("/Users/croe/Desktop/test_reads.fasta");
+    if (inReadsFile.is_open()) {
+        while (!inReadsFile.eof()) {
+            while (getline(inReadsFile, line)) {
+                if (line[0] != '>') {
+                    int counter = 0;
+                    while (counter < steppingSize) {
+                        subLineToAddToNode = line.substr(counter, kmerSize);
+                        readNode * newReadNode = CreateReadNode();
+                        newReadNode->readSeedSequence = subLineToAddToNode;
+                        //cout<<"read kmer sequence: "<<subLineToAddToNode<<"\n";
+                        newReadNode->readNumber= readNumber;
+                        //cout<<"read number: "<<readNumber<<"\n";
+                        counter++;
+                    }
+                    readNumber++;
+                }
+            }
+        }
+    }
+    inReadsFile.close();
+    return readsFile;
 }
 
 //destructor

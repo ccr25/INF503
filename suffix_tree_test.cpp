caccded1e20821251ac712acd588c6014006f5dc
hp263 <32024225+hp263@users.noreply.github.com>
2017-12-12 08:22:09 +0800

    works !!

--- suffix_tree_test.cpp
+++ suffix_tree_test.cpp
@@ -44,7 +44,7 @@ public:
     static void build_tree(suffixNode *root,char * contig);
     //void ~suffix_tree();
     static void insert(struct suffixNode *root, const char *contig,const char *geno,int position ) ;
-    static bool search(struct suffixNode *root, const char *contig );
+    static bool search(struct suffixNode *root, const char *contig,const char *geno);
     static suffixNode * createNode();
 
 };
@@ -79,54 +79,96 @@ suffixNode * Suffix_Tree::createNode()
 void Suffix_Tree::insert(struct suffixNode *root, const char *sequence,const char *geno, int position) {
 	
     int i,j;
-	
+	int substr_len;
     int length = strlen(sequence);
+    int gothrough = 0;
     int index,index1,index2;
     struct suffixNode *p = root;
     cout<<" position is "<< position -1  <<"\n";
-    for (i = 0; i < length; i++)
+    while (gothrough<length)
     {
 
-        index = chartoASCII(sequence[i]);
+    	//if we don't have the node, we create a new one and store start, end in this node
+        index = chartoASCII(sequence[gothrough]);
         if (!p->children[index]){
             p->children[index] = createNode();
             p=p->children[index];
-            p->	start = position+i-1;
-            p-> end = position +i+length-2;
+            p->	start = position+gothrough-1;
+            p-> end = position +length-2;
             cout<<"I got a new node lol"<<"\n";
             return;
         }else{
+        // if we do have the node, we compare them
 		int *temp;
-		
 		p=p->children[index];
 		int size_of_nodestr=p->end-p->start+1;
 		char node_str[size_of_nodestr];
 		snprintf(node_str,size_of_nodestr+1,geno+p->start);
 		cout<<"node str is "<<node_str<<"\n";
-		if (size_of_nodestr<length){
-			for (j=1;j<size_of_nodestr;j++){
-				if (node_str[j]!=sequence[j]){
+		substr_len=length-gothrough;
+		cout<<"we there #1"<<"\n";
+		if (size_of_nodestr<substr_len){
+			for (j=0;j<size_of_nodestr;j++){
+				if (node_str[j]!=sequence[gothrough+j]){
 					//char temp_substr1[length-j];
 					//char temp_substr2[size_of_nodestr-j];
+					cout<<"we there #2"<<"\n";
 					int previous_end;
-					p->end=p->start+j-1;
+					
+					p->end=p->start+gothrough+j-1;
 					previous_end = p->end+1;
-					index1=chartoASCII(node_str[j]);
-					index2=chartoASCII(sequence[j]);
+					cout<<"previous_end "<<previous_end<<"\n";
+					index1=chartoASCII(node_str[gothrough+j]);
+					index2=chartoASCII(sequence[gothrough+j]);
 					p->children[index1] = createNode();
 					struct suffixNode *temp1 = p->children[index1];
 					temp1->start=previous_end;
 					temp1->end =previous_end+size_of_nodestr-j-1;
 					p->children[index2] = createNode();
 					struct suffixNode *temp2 = p->children[index2];
-					temp2->start=j;
-					temp2->end =length-1;
+					cout<<"j is "<<j<<"\n";
+					cout<<"length is "<<length<<"\n";
+					temp2->start=position-1+gothrough+j ;
+					temp2->end =position+length-2;
 					return;
 				}
 			}
+			
+		gothrough=gothrough+size_of_nodestr;
+
+		} else{
+			cout<<"we there #3"<<"\n";
+			for (j=0;j<substr_len;j++){
+				if (node_str[j]!=sequence[gothrough+j]){
+					//char temp_substr1[length-j];
+					//char temp_substr2[size_of_nodestr-j];
+					int previous_end;
+					p->end=p->start+gothrough+j-1;
+					previous_end = p->end+1;
+					index1=chartoASCII(node_str[gothrough+j]);
+					index2=chartoASCII(sequence[gothrough+j]);
+					p->children[index1] = createNode();
+					struct suffixNode *temp1 = p->children[index1];
+					temp1->start=previous_end;
+					temp1->end =previous_end+size_of_nodestr-j-1;
+					p->children[index2] = createNode();
+					struct suffixNode *temp2 = p->children[index2];
+					cout<<"j is "<<j<<"\n";
+					cout<<"length is "<<length<<"\n";
+					temp2->start=position-1+gothrough+j ;
+					temp2->end =position+length-2;
+					return;
+					}				
+				
+			}
+			return;
 		}
-		
-		//cout<<"childd start "<<p->	start<<"\n";
+
+		}
+		}
+		 
+    p->isend = true;
+    		//cout<<"childd start "<<p->	start<<"\n";
 		//cout<<"childd end "<<p->	end<<"\n";
     	/*p-> end = position +i+length-2;
     	cout<<" start is "<< p->start <<"\n";
@@ -134,9 +176,6 @@ void Suffix_Tree::insert(struct suffixNode *root, const char *sequence,const cha
         cout<<"we didn't have a new node'"<<"\n";
 		
 		cout<<"next node start"<<p->start <<"\n";*/
-	}
-    }
-    p->isend = true;
 }
 
 //destructor
@@ -146,7 +185,7 @@ void Suffix_Tree::insert(struct suffixNode *root, const char *sequence,const cha
 
 
 // search a sequence if it is presneted in the tree or not. return 0 for not presented, 1 for presented;
-bool Suffix_Tree::search(struct suffixNode *root, const char *sequence)
+/*bool Suffix_Tree::search(struct suffixNode *root, const char *sequence)
 {
     int i;
     int count = 0;
@@ -166,8 +205,56 @@ bool Suffix_Tree::search(struct suffixNode *root, const char *sequence)
         p = p->children[index];
     }
     return true;         //(p!= NULL && p->isend);
-}
+}*/ 
 
+bool Suffix_Tree::search(struct suffixNode *root, const char *sequence,const char *geno)
+{
+    int i;
+    int length = strlen(sequence);
+    int index;
+	int gothrough=0;
+    struct suffixNode *p = root;
+    while (gothrough<length)
+    {
+    	cout<<"go through is "<<gothrough<<"\n";
+        index = chartoASCII(sequence[gothrough]);
+        cout<<"we there #3  "<<"\n";
+        if (!p->children[index]) {
+        		cout<<"we there #2  "<<"\n";
+                return false;
+            }else {
+            	cout<<"we there #1 "<<"\n";
+            	p=p->children[index];
+            	
+            	int size_of_nodestr=p->end - p->start +1;
+            	int size_of_substr=length-gothrough;
+            	
+            	char node_str[size_of_nodestr];
+            	snprintf(node_str,size_of_nodestr+1,geno+p->start);
+            	//cout<<"node string is "<<node_str<<"\n";
+            	if (size_of_nodestr<size_of_substr){
+            		for (i=0;i<size_of_nodestr;i++){
+            			if (node_str[i]!=sequence[gothrough+i]){
+            			return false;
+						}
+					}
+					gothrough=gothrough+size_of_nodestr;
+					
+				} else{
+					for (i=0;i<size_of_substr;i++){
+            			if (node_str[i]!=sequence[gothrough+i]){
+            			return false;
+						}
+					}
+					return true;
+				}
+            	
+			}
+        cout<<"we there #4 "<<"\n";
+        
+    }
+    return true;         //(p!= NULL && p->isend);
+}
 
 int main(){
  	char s[15000];
@@ -188,19 +275,24 @@ int main(){
 	}	
 	//cout<<x<<"\n";
 	
-	char tester2[]= "TTCGAG";
+	char tester2[]= "ATCCATCG";
 	//snprintf(tester,17,tester2);
 	//cout<<tester<<"\n";
 
-	 char * subStr = "TACG";
+	 char * subStr = "GCCAT";
 	suffixNode * root= Suffix_Tree::createNode();
 	Suffix_Tree::build_tree(root,tester2);
-	cout<<"root start "<<root->children[1]->start<<"\n";
-	cout<<"root end  "<<root->children[1]->end<<"\n";
-	cout<<"root start "<<root->children[2]->children[0]->start<<"\n";
-	cout<<"root end  "<<root->children[1]->children[0]->end<<"\n";
-	/*cool = Suffix_Tree::search(root, subStr);
-	cout<<cool<<"\n";*/
+	// A for 0, C for 1, G for 2, T for 3.
+//	cout<<"root start first generation "<<root->children[3]->start<<"\n";
+//	cout<<"root end first generation "<<root->children[3]->end<<"\n";
+//	cout<<"root start second generation "<<root->children[3]->children[2]->start<<"\n";
+//	cout<<"root end second generation "<<root->children[3]->children[2]->end<<"\n";
+//	cout<<"root start third generation"<<root->children[1]->children[1]->children[1]->start<<"\n";	
+//	cout<<"root end third generation "<<root->children[1]->children[1]->children[1]->end<<"\n";
+//	cout<<"root start forth generation"<<root->children[1]->children[1]->children[1]->children[1]->start<<"\n";
+//	cout<<"root start forth generation"<<root->children[1]->children[1]->children[1]->children[1]->end<<"\n";
+	cool = Suffix_Tree::search(root, subStr,tester2);
+	cout<<cool<<"\n";
 	
     
     
